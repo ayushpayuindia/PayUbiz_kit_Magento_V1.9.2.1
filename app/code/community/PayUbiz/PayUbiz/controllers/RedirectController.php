596d42c867bd53c425500ddca4d9316ff4ff805b
PayU Kit developer <ayush.mittal@payu.in>
2017-10-24 13:34:30 +0530

    Update RedirectController.php

--- app/code/community/PayUbiz/PayUbiz/controllers/RedirectController.php
+++ app/code/community/PayUbiz/PayUbiz/controllers/RedirectController.php
@@ -2,13 +2,8 @@
 /**
  * RedirectController.php
  * 
- * Copyright (c) 2010-2011 PayU India
- * 
- * 
- * 
- * 
- * @author     Ayush Mittal
- * @copyright  2011-2015 PayU India
+ * @author     Payubiz
+ * @copyright  2011-2017 PayU India
  * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
  * @link       http://www.payu.in
  * @category   PayUbiz
